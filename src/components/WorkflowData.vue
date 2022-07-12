c1f34590f3fc0c787593e7ba727c6ca8ad5d7380
曾祥超 <zengxiangchao@do1.com.cn>
2022-07-12 10:23:49 +0800

    完成自定义node

---
+++ src/components/WorkflowData.vue
@@ -0,0 +1,29 @@
+<script>
+	export default {
+		data() {
+			return {
+				// 节点
+				nodes: [{
+						id: 'node1', // String，可选，节点的唯一标识						
+						x: 40, // Number，必选，节点位置的 x 值
+						y: 40, // Number，必选，节点位置的 y 值
+						shape: 'custom-rect',
+						title2: 'hello', // String，节点标签
+					},
+					{
+						id: 'node2', // String，节点的唯一标识
+						x: 160, // Number，必选，节点位置的 x 值
+						y: 180, // Number，必选，节点位置的 y 值						
+						shape: 'custom-rect',
+						title2: 'word', // String，节点标签: 'world', // String，节点标签
+					},
+				],
+				// 边
+				edges: [{
+					source: 'node1', // String，必须，起始节点 id
+					target: 'node2', // String，必须，目标节点 id
+				}, ]
+			}
+		}
+	}
+</script>
---
+++ src/components/workflow.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="container"></div>
+	<button @click="add">外部 Add</button>
+</template>
+
+<script>
+	import {
+		Graph
+	} from '@antv/x6'
+	import data from './WorkflowData.vue'
+	import './MyNode.js'
+	export default {
+		name: "WorkFlow",
+		mixins: [data],
+		methods: {
+			initGraph: function() {
+
+				this.graph = new Graph({
+					container: document.getElementById('container'),
+					width: 800,
+					height: 600,
+					grid: true,
+				});
+			},
+			createNodes: function() {
+				this.nodes.forEach(node => {
+					this.graph.addNode(node)
+				})
+
+			},
+			add: function() {
+				const nodes = this.graph.getNodes();				
+				if (nodes.length) {
+					nodes.forEach((node) => {
+						node.setAttrByPath("title/text",Math.random())
+						console.log(node.shape)
+					});
+				}
+			}
+		},
+		mounted: function() {
+			this.initGraph()
+			this.createNodes()
+		}
+	}
+</script>
+
+<style>
+</style>
